8e7558af6f566b59664227897300d1fb22d4868c
jessieh9 <104229166+jessieh9@users.noreply.github.com>
2022-12-16 19:08:15 -0800

    day 6

--- day6/pt2.py
+++ day6/pt2.py
@@ -2,10 +2,10 @@ f = open("day6/input.txt")
 contents = f.readlines()
 line = contents[0].rstrip()
 
-num = 4
+num = 14
 while True:
-    # set because it will remove duplicates - no duplicates means len of 4
-    if len(set(line[:4])) != 4:  # bvwbjplb gvbhsrlpgdmjqwftvncz
+    # set because it will remove duplicates - no duplicates means len of 14
+    if len(set(line[:14])) != 14:
         line = line[1:]
         num += 1
     else:
